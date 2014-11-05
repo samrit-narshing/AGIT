ebd2f45388c0c1db68dce1a79b93b0df4a247a03
Samrit <Samrit@sam-pc>
2014-11-05 22:57:12 +0545

    comment1

--- src/agit/AGIT.java
+++ src/agit/AGIT.java
@@ -15,7 +15,7 @@ public class AGIT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("AAAA");
+        System.out.println("AAAAa");
         // TODO code application logic here
     }
     
